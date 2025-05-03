dce0bfee4eb0628602b1836d165a36d9035e8c81
Valentin <latronixo@gmail.com>
2025-05-03 17:11:55 +0300

    25. Сделал нижнюю панель навигейшнконтроллера, запретил переход на сторонние сайты, настроил ProgressView

---
+++ 24.Project 4 (WebKit)/TableViewController.swift
@@ -0,0 +1,12 @@
+//
+//  TableViewController.swift
+//  24.Project 4 (WebKit)
+//
+//  Created by Валентин Картошкин on 03.05.2025.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+}
--- 24.Project 4 (WebKit)/ViewController.swift
+++ 24.Project 4 (WebKit)/ViewController.swift
@@ -11,6 +11,10 @@ import WebKit
 class ViewController: UIViewController, WKNavigationDelegate {
     
     var webView: WKWebView!
+    var progressView: UIProgressView!
+    
+    //список разрешенных вебсайтов, по которым пользователь может переходить в нашем приложении
+    var websites = ["apple.com", "hackingwithswift.com"]
     
     override func loadView() {
         webView = WKWebView()
@@ -24,16 +28,38 @@ class ViewController: UIViewController, WKNavigationDelegate {
         //создадим кнопку в навигейшнконтроллере
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
         
+        //создаем тулбар (встроен в навигейшнконтроллер)
+        //создаем пустое пространство, занимающее столько места, сколько он может, притесняя остальные элементы внутри его контейнера
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        //создаем кнопку обновить
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+
+        //создаем прогрессвью
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.sizeToFit()
+        let progressButton = UIBarButtonItem(customView: progressView)
+        
+        //добавляем их в тулбар
+        toolbarItems = [progressButton, spacer, refresh]
+        //делаем его видимым
+        navigationController?.isToolbarHidden = false
+        
+        //добавляем наблюдателя для прогрессвью
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+        
         //загрузим адрес сайта
-        let url = URL(string: "https://www.hackingwithswift.com")!
+        let url = URL(string: "https://" + websites[0])!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
     }
 
     @objc func openTapped() {
         let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
-        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
-        ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage))
+        
+        for website in websites {
+            ac.addAction(UIAlertAction(title: website, style: .default, handler: openPage))
+        }
+        
         ac.addAction(UIAlertAction(title: "Cancel", style: .default))
         ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
         present(ac, animated: true)
@@ -50,5 +76,34 @@ class ViewController: UIViewController, WKNavigationDelegate {
         title = webView.title
     }
     
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+           progressView.progress = Float(webView.estimatedProgress)
+       }
+    }
+    
+    //событие перехода на новую страницу внутри webView (разрешено переходить только по заданному списку сайтов)
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        
+        //для удобства получаем url в отдельной константе
+        let url = navigationAction.request.url
+        
+        //извлекаем host (не в каждом url есть host)
+        if let host = url?.host {
+            //цикл по разрешенным вебсайтам
+            for website in websites {
+                //если host, на который переходит пользователь, входит в список разрешенных вебсайтов
+                if host.contains(website) {
+                    //то разрашаем переход
+                    decisionHandler(.allow)
+                    //и выходим из функции
+                    return
+                }
+            }
+        }
+        
+        //если же host, на который переходит пользователь не входит в список разрешенных вебсайтов, то запрещаем этот переход
+        decisionHandler(.cancel)
+    }
 }
 
